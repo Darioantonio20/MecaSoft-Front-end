e1fee89920c62f0fde1df2e8fcd332802b195d1d
Darío Antonio Gutiérrez Álvarez <105560538+Darioantonio20@users.noreply.github.com>
2024-02-04 22:24:09 -0600

    [ADD] mision

---
+++ MecaSoft/src/molecules/Mision.jsx
@@ -0,0 +1,15 @@
+import "../assets/style/Mision.css";
+
+function Section3() {
+    return ( 
+        <>
+            <div className="section3">
+                <h2 className="title">Nuestra Misión</h2>
+                <p className="text">En la Universidad Politécnica de Chiapas, tenemos la misión de fomentar aprendizajes para nuestros profesionales competentes de Ingeniería Mecatrónica a través de un programa de estudio de ingeniería, en un ambiente seguro e intuitivo para mejorar el desarrollo de soluciones en sectores empresariales y científico-tecnológico.</p>
+                <p className="highlight">Esto nos ha llevado a mejorar nuestros métodos de enseñanza implementando software que facilite el aprendizaje.</p>
+            </div>
+        </>
+     );
+}
+
+export default Section3;
--- MecaSoft/src/pages/LandingPage.jsx
+++ MecaSoft/src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import NavBar from "../atoms/NavBar";
 import Footer from "../atoms/Footer";
+import Mision from "../molecules/Mision";
 import Section1 from "../molecules/Section1";
 import Section2 from "../molecules/Section2";
 
@@ -7,6 +8,7 @@ function LandingPage() {
     return ( 
         <>
         <NavBar/>
+        <Mision/>
         <Section1/>    
         <Section2/>    
         <Footer/>
