00de5feb425a48a262311a92d013711c906d6510
Darío Antonio Gutiérrez Álvarez <105560538+Darioantonio20@users.noreply.github.com>
2024-02-04 18:56:09 -0600

    [FIX] reloader y imagen de registro

--- MecaSoft/src/pages/InicioDeSesion.jsx
+++ MecaSoft/src/pages/InicioDeSesion.jsx
@@ -1,79 +1,126 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBar from "../atoms/NavBar";
 import ImgLogoMecaSoftUp from "../assets/img/LogoMecaSoftUp.png";
-import "../assets/style/InicioDeSesion.css";
+import "../assets/style/Forms.css";
+import "../assets/style/Reloader.css";
 
 function InicioDeSesion() {
-
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
 
     const redirectToRegister = () => {
         navigate('/registro');
-      };
+    };
 
-    return ( 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        // Aquí puedes agregar la lógica para enviar los datos del formulario
+        // Cuando los datos se hayan enviado, puedes establecer setIsLoading(false)
+
+        // Agrega esto para cambiar isLoading a false después de 3 segundos
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1500);
+    };
+
+    return (
         <>
-        <NavBar/>
-            <div className="h-100 gradient-form divPadre">
-                <div className="container py-5 h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col-xl-10">
-                            <div className="card rounded-3 text-black">
-                                <div className="row g-0">
-                                    <div className="col-lg-6">
-                                        <div className="card-body p-md-5 mx-md-4">
-                                            <div className="text-center">
-                                                <img src={ImgLogoMecaSoftUp} loading='lazy' width={185} alt="logo"/>
-                                                <h4 className="mt-1 mb-5 pb-1 typing-effect">Llene el siguiente formulario.</h4>
-                                            </div>
-                                            <form className=''>
-                                                
-                                                <div class="form__group_one mt-5 mb-5">
-                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text"/>
-                                                    <label for="name" class="form__group_oneform__label"> Nombre de usuario </label>
-                                                </div>
-                                                <div class="form__group_one mb-5">
-                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text"/>
-                                                    <label for="name" class="form__group_oneform__label"> Contraseña </label>
-                                                </div>
-                                                {/*<div className="text-center pt-1 mb-5 pb-1">
-                                                    <a className="text-muted" href="#!">Olvidaste tu contraseña?</a>
-                                                </div>*/}
-                                                <div className='text-center d-flex justify-content-center mt-5 mb-5'>
-                                                    <button style={{border: 'none', backgroundColor: 'transparent'}}>
-                                                        <div className="container-button mt-3">
-                                                            <div className="hover bt-1"></div>
-                                                            <div className="hover bt-2"></div>
-                                                            <div className="hover bt-3"></div>
-                                                            <div className="hover bt-4"></div>
-                                                            <div className="hover bt-5"></div>
-                                                            <div className="hover bt-6"></div>
-                                                            <button className='buttonForm'></button>
+            {isLoading ? (
+                <div className="loading-overlay">
+                    <div className="gearbox">
+                        <div className="gear one">
+                            <div className="gear-inner">
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                            </div>
+                        </div>
+                        <div className="gear two">
+                            <div className="gear-inner">
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                            </div>
+                        </div>
+                        <div className="gear three">
+                            <div className="gear-inner">
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                            </div>
+                        </div>
+                        <div className="gear four large">
+                            <div className="gear-inner">
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <NavBar/>
+                    <div className="h-100 gradient-form divPadre">
+                        <div className="container py-5 h-100">
+                            <div className="row d-flex justify-content-center align-items-center h-100">
+                                <div className="col-xl-10">
+                                    <div className="card rounded-3 text-black">
+                                        <div className="row g-0">
+                                            <div className="col-lg-6">
+                                                <div className="card-body p-md-5 mx-md-4">
+                                                    <div className="text-center">
+                                                        <img src={ImgLogoMecaSoftUp} loading='lazy' width={185} alt="logo"/>
+                                                        <h4 className="mt-1 mb-5 pb-1 typing-effect">Llene el siguiente formulario.</h4>
+                                                    </div>
+                                                    <form onSubmit={handleSubmit}>
+                                                        <div class="form__group_one mt-5 mb-5">
+                                                            <input type="text" class="form__group_oneform__field w-100" placeholder="Input text" required/>
+                                                            <label for="name" class="form__group_oneform__label"> Nombre de usuario </label>
                                                         </div>
-                                                    </button>
-                                                        
+                                                        <div class="form__group_one mb-5">
+                                                            <input type="text" class="form__group_oneform__field w-100" placeholder="Input text" required/>
+                                                            <label for="name" class="form__group_oneform__label"> Contraseña </label>
+                                                        </div>
+                                                        <div className='text-center d-flex justify-content-center mt-5 mb-5'>
+                                                            <button style={{border: 'none', backgroundColor: 'transparent'}}>
+                                                                <div className="container-button mt-3">
+                                                                    <div className="hover bt-1"></div>
+                                                                    <div className="hover bt-2"></div>
+                                                                    <div className="hover bt-3"></div>
+                                                                    <div className="hover bt-4"></div>
+                                                                    <div className="hover bt-5"></div>
+                                                                    <div className="hover bt-6"></div>
+                                                                    <button className='buttonForm'></button>
+                                                                </div>
+                                                            </button>
+                                                        </div>
+                                                        <div className="d-flex align-items-center justify-content-center pb-4">
+                                                            <p className="mb-0 me-2">No tienes cuenta?</p>
+                                                            <button onClick={redirectToRegister} type='button' className="btn btn-outline-danger">Crea una</button>
+                                                        </div>
+                                                    </form>
                                                 </div>
-                                                <div className="d-flex align-items-center justify-content-center pb-4">
-                                                    <p className="mb-0 me-2">No tienes cuenta?</p>
-                                                    <button onClick={redirectToRegister} type='button' className="btn btn-outline-danger">Crea una</button>
+                                            </div>
+                                            <div className="col-lg-6 d-flex align-items-center gradient-custom-2 red-background">
+                                                <div className="text-white px-3 py-4 p-md-5 mx-md-4">
                                                 </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 d-flex align-items-center gradient-custom-2 red-background">
-                                        <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                                            <h4 className="mb-4">MecaSoft</h4>
-                                            <p className="small mb-0">Patrocinador de exitos 20-21</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </>
+            )}
         </>
-     );
+    );
 }
 
 export default InicioDeSesion;
--- MecaSoft/src/pages/Registro.jsx
+++ MecaSoft/src/pages/Registro.jsx
@@ -1,17 +1,71 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBar from "../atoms/NavBar";
 import ImgLogoMecaSoftUp from "../assets/img/LogoMecaSoftUp.png";
-import "../assets/style/InicioDeSesion.css";
+import ImgFormLadoDerecho from "../assets/img/imgFormLadoDerecho.svg";
+import "../assets/style/Forms.css";
+import "../assets/style/Reloader.css";
 
 function Registro() {
 
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
 
     const redirectToLogin = () => {
         navigate('/inicioDeSesion');
       };
 
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        // Aquí puedes agregar la lógica para enviar los datos del formulario
+        // Cuando los datos se hayan enviado, puedes establecer setIsLoading(false)
+
+        // Agrega esto para cambiar isLoading a false después de 3 segundos
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1500);
+    };
+    
     return ( 
+        <>
+        {isLoading ? (
+                <div className="loading-overlay">
+                    <div className="gearbox">
+                        <div className="gear one">
+                            <div className="gear-inner">
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                            </div>
+                        </div>
+                        <div className="gear two">
+                            <div className="gear-inner">
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                            </div>
+                        </div>
+                        <div className="gear three">
+                            <div className="gear-inner">
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                            </div>
+                        </div>
+                        <div className="gear four large">
+                            <div className="gear-inner">
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : (
         <>
          <NavBar/>
             <div className="h-100 gradient-form divPadre">
@@ -26,30 +80,29 @@ function Registro() {
                                                 <img src={ImgLogoMecaSoftUp} loading='lazy' width={185} alt="logo"/>
                                                 <h4 className="mt-1 mb-5 pb-1 typing-effect">Llene el siguiente formulario.</h4>
                                             </div>
-                                            <form className=''>
-                                                
+                                            <form onSubmit={handleSubmit}>
                                                 <div class="form__group_one mt-5 mb-5">
-                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text"/>
+                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text" required/>
                                                     <label for="name" class="form__group_oneform__label"> Nombre completo </label>
                                                 </div>
                                                 <div class="form__group_one mt-5 mb-5">
-                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text"/>
+                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text" required/>
                                                     <label for="name" class="form__group_oneform__label"> Nombre de usuario </label>
                                                 </div>
                                                 <div class="form__group_one mt-5 mb-5">
-                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text"/>
+                                                    <input type="email" class="form__group_oneform__field w-100" placeholder="Input text" required/>
                                                     <label for="name" class="form__group_oneform__label"> Correo electrónico  </label>
                                                 </div>
                                                 <div class="form__group_one mt-5 mb-5">
-                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text"/>
+                                                    <input type="number" class="form__group_oneform__field w-100" placeholder="Input text" required/>
                                                     <label for="name" class="form__group_oneform__label"> Matrícula </label>
                                                 </div>
                                                 <div class="form__group_one mt-5 mb-5">
-                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text"/>
+                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text" required/>
                                                     <label for="name" class="form__group_oneform__label"> Contraseña </label>
                                                 </div>
                                                 <div class="form__group_one mb-5">
-                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text"/>
+                                                    <input type="text" class="form__group_oneform__field w-100" placeholder="Input text" required/>
                                                     <label for="name" class="form__group_oneform__label"> Confirmar contraseña </label>
                                                 </div>
                                                 {/*<div className="text-center pt-1 mb-5 pb-1">
@@ -76,10 +129,11 @@ function Registro() {
                                             </form>
                                         </div>
                                     </div>
-                                    <div className="col-lg-6 d-flex align-items-center gradient-custom-2 red-background">
-                                        <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                                            <h4 className="mb-4">MecaSoft</h4>
-                                            <p className="small mb-0">Patrocinador de exitos 20-21</p>
+                                    <div className="col-lg-6 d-flex align-items-center justify-content-center gradient-custom-2 red-background">
+                                        <div className="text-white px-3 py-4 p-md-5 mx-md-4 text-center">
+                                            <h1 className="mb-4 display-6 display-md-5 display-lg-4">MecaSoft</h1>
+                                            <p className="animated-text mb-0 fs-5 fs-md-4 fs-lg-3">Los ingenieros mecatrónicos diseñan, integran y desarrollan diversos productos</p>
+                                            <img src={ImgFormLadoDerecho} loading='lazy' className="img-fluid" width={500} alt="Img Form"/>
                                         </div>
                                     </div>
                                 </div>
@@ -88,6 +142,8 @@ function Registro() {
                     </div>
                 </div>
             </div>
+            </>
+            )}
         </>
      );
 }
